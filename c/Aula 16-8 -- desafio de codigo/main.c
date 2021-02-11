1f835efaa994ac12bba3f10d42ade2e53da615c1
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-11 20:48:20 -0300

    Aula 16-8 -- Desafio concluido

--- c/Aula 16-8 -- desafio de codigo/main.c
+++ c/Aula 16-8 -- desafio de codigo/main.c
@@ -15,22 +15,20 @@ int main()
     printf("\n");
 
     printf("Informe a porcentagem de decrecimo: ");
-    scanf("%d", &porcentagemDecrecimo);
+    scanf("%f", &porcentagemDecrecimo);
     printf("\n");
 
     decrecimo = (porcentagemDecrecimo / 100);
     parcela = parcelaInicial;
 
-    printf("%f", decrecimo);
-
-    /*for (int i = 1; i <= numDeParcelas; i++){
+    for (int i = 1; i <= numDeParcelas; i++){
         if (i > 1){
-            parcela = parcela - (parcela * 0.01);
+            parcela = parcela - (parcela * decrecimo);
             printf("Parcela %d de %d: R$ %.2f \n", i, numDeParcelas, parcela);
         } else {
             printf("Parcela %d de %d: R$ %.2f \n", i, numDeParcelas, parcelaInicial);
         }
-    }*/
+    }
 
     return 0;
 }
