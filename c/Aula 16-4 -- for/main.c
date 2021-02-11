ff9af67b8452487fd82c5e79d32c628d5bb513e9
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-11 10:55:06 -0300

    Aula 16-4 -- for()

--- c/Aula 16-4 -- for/main.c
+++ c/Aula 16-4 -- for/main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int tab;
+
+    printf("Tabuada! \n");
+
+    printf("Informe qual tabuada deseja: ");
+    scanf("%d", &tab);
+
+    printf("\n");
+
+    for (int i = 0; i <= 10; i++ ) {
+        printf("%d X %d = %d \n", tab, i, tab * i);
+    }
     return 0;
 }
