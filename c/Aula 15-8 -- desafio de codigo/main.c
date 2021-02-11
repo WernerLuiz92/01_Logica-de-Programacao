9b5992fd0c8fe91f9026f9637d7132ecf1f55f29
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-11 09:52:46 -0300

    Aula 15-8 Desafio concluido

--- c/Aula 15-8 -- desafio de codigo/main.c
+++ c/Aula 15-8 -- desafio de codigo/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    int essenciais, prioridades, estiloDeVida;
+
+    scanf("%d", &essenciais);
+    scanf("%d", &prioridades);
+    scanf("%d", &estiloDeVida);
+
+    if (essenciais <= 50) {
+        printf("Gastos essenciais: dentro da regra");
+    } else {
+        printf("Gastos essenciais: acima da regra");
+    }
+    if (prioridades <= 15) {
+        printf("Prioridades financeiras: dentro da regra");
+    } else {
+        printf("Prioridades financeiras: acima da regra");
+    }
+    if (essenciais <= 35) {
+        printf("Estilo de vida: dentro da regra");
+    } else {
+        printf("Estilo de vida: acima da regra");
+    }
+
     return 0;
 }
