896fc3c866056cb5356a9ed536434023a7e1bdb1
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-12 16:54:36 -0300

    Aula 18-1 -- Exercício completado

--- c/Aula 18-5 -- hora de praticar/main.c
+++ c/Aula 18-5 -- hora de praticar/main.c
@@ -1,8 +1,21 @@
+//Exercício 5: Escreva um programa em C que emita o som de miados
+//durante x vezes, quantidade de vezes esta que deverá ser informada
+//previamente pelo usuário. Considere como emissão de um miado a escrita no
+//Console da palavra “MIAU”.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int miar, i;
+
+    printf("Quantas vezes? ");
+    scanf("%d", &miar);
+
+    for (i = 0; i < miar; i++){
+        printf("MIAU \n");
+    }
+
     return 0;
 }
