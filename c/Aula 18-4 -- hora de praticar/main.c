8535bc2d050fde83aca13896b2504b3b221a3127
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-12 16:28:44 -0300

    Aula 18-4 -- Exercício completado

--- c/Aula 18-4 -- hora de praticar/main.c
+++ c/Aula 18-4 -- hora de praticar/main.c
@@ -1,8 +1,25 @@
+//Exercício 4: Escreva um programa em C que faça com que um gato dê um miado
+//com uma intensidade informada pelo usuário. Enquanto o usuário não digitar “PARAR”,
+//o personagem deverá continuar solicitado “níveis de miado” e os emitindo no nível
+//pedido pelo usuário. Considere como intensidade do miado a quantidade de letras “I”
+//na palavra “MIAU”. Por exemplo, se o usuário informar 6 como intensidade, deverá ser
+//escrito “MIIIIIIAU”; se o usuário informar 3 como intensidade, deverá ser escrito “MIIIAU”.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int intensidade, i;
+
+    printf("Qual a intensidade do miado? ");
+    scanf("%d", &intensidade);
+
+    printf("M");
+    for (i = 0; i < intensidade; i++){
+        printf("I");
+    }
+    printf("AU");
+
     return 0;
 }
