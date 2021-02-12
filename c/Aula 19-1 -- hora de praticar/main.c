4effb0d36227ebe12a0b3b1b3c77f3395aaadc3c
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-12 17:52:26 -0300

    .

--- c/Aula 19-1 -- hora de praticar/main.c
+++ c/Aula 19-1 -- hora de praticar/main.c
@@ -1,8 +1,25 @@
+//Exercício 1: Escreva um programa em C que forneça o fatorial de um
+//número fornecido pelo usuário. Para lembrar: por exemplo, o fatorial
+//de 2 (2!) é igual 2 * 1, que é igual a 2. O fatorial de 4 (4!) é
+//igual a 4 * 3 * 2 * 1, que é igual a 24.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int f, i;
+
+    printf("Informe o fatorial desejado: ");
+    scanf("%d", &f);
+
+    for (f = 1; i > 0; i--){
+        f = f * i;
+        printf("%d\n ", f);
+    }
+
+    printf("%d", f);
+
+
     return 0;
 }
