b5a56ce2212cfb6dc48e1ed4dda2af06e4e9aede
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-12 15:30:00 -0300

    Aula 18-1 -- Exercício completado

--- c/Aula 18-1 -- hora de praticar/main.c
+++ c/Aula 18-1 -- hora de praticar/main.c
@@ -1,8 +1,24 @@
+//Exercício 1: Escreva um programa em C que consiga ler um valor em dólares e
+//devolva para o usuário este valor convertido em reais.
+//Considere que US$ 1,00 = R$ 3,85.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    float valorUSD, valorBRL, cotacao;
+
+    printf("Informe um valor em $ dolares: ");
+    scanf("%f", &valorUSD);
+
+    printf("Informe a cotacao do dia: ");
+    scanf("%f", &cotacao);
+
+    valorBRL = valorUSD * cotacao;
+
+    printf("\n\n");
+
+    printf("O valor US$ %.2f e equivalente a R$ %.2f", valorUSD, valorBRL);
     return 0;
 }
