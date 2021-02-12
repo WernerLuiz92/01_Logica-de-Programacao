b77c48f202bd7df4e62231502ba9881010320581
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-11 23:57:32 -0300

    Aula 17-8 -- Desafio conluído

--- c/Aula 17-8 -- desafio de codigo/main.c
+++ c/Aula 17-8 -- desafio de codigo/main.c
@@ -3,6 +3,58 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int qtdFamilias = 0, i;
+    float soma = 0;
+
+    printf("Quantas famílias foram entrevistadas? ");
+    scanf("%d", &qtdFamilias);
+    printf("\n");
+
+    float rendaFamilias[qtdFamilias];
+
+    for (i = 0; i < qtdFamilias; i++){
+        printf("Informe a renda da família: ");
+        scanf("%f", &rendaFamilias[i]);
+    }
+
+    printf("\n");
+
+    //Ordena o array
+    int alteracoes = 1;
+    while (alteracoes > 0){
+        alteracoes = 0;
+        for (i = 0; i < (qtdFamilias -1); i++){
+            if (rendaFamilias[i] > rendaFamilias[i + 1]){
+                float temp = rendaFamilias[i];
+                rendaFamilias[i] = rendaFamilias[i + 1];
+                rendaFamilias[i + 1] = temp;
+
+                alteracoes = alteracoes + 1;
+            }
+        }
+    }
+
+    printf("Rendas familiares em order crescente: \n");
+
+    for (i = 0; i < qtdFamilias; i++){
+        printf("Família %d - Renda: R$ %.2f \n", i + 1, rendaFamilias[i]);
+    }
+
+    printf("\n");
+
+    //Calcula a média
+    for (i = 0; i < qtdFamilias; i++){
+
+        soma = soma + rendaFamilias[i];
+
+    }
+
+    float media = soma / qtdFamilias;
+
+    printf("Média - %.2f \n", media);
+    printf("Maior - %.2f \n", rendaFamilias[qtdFamilias - 1]);
+    printf("Menor - %.2f \n", rendaFamilias[0]);
+
+
     return 0;
 }
