42593699298c8a2955eb35d53319d20ba55a3fbd
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-12 14:09:12 -0300

    Update main.c

--- c/Aula 17-10 -- desafio de codigo/main.c
+++ c/Aula 17-10 -- desafio de codigo/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int qtdAportes;
+    float porcentagemJuros;
+
+    printf("Informe a quantidade de aportes: ");
+    scanf("%d", &qtdAportes);
+
+    printf("Informe a porcentagem de juros: ");
+    scanf("%d", &qtdAportes);
+
+    float valorAporte[qtdAportes];
+
+    for (i = 0; i < qtdAportes; i++){
+
+    }
+
     return 0;
 }
