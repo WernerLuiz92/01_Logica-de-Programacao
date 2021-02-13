758d5ceb6a9dabf372ae52eff7fc7673a2f0d626
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-13 09:38:41 -0300

    .

--- c/Aula 19-3 -- hora de praticar/main.c
+++ c/Aula 19-3 -- hora de praticar/main.c
@@ -1,8 +1,50 @@
+//Exercício 3: Escreva um programa em C que, através da leitura
+//de um número fornecido pelo usuário, gere a sequência de Fibonacci
+//com a quantidade de termos indicada pelo usuário.
+//A sequência de Fibonacci, basicamente, consiste na geração do número
+//seguinte através da soma dos dois números anteriores...
+//Por exemplo: 1, 1, 2 (1 + 1), 3 (1 + 2), 5 (3 + 2), e assim por diante.
+//Por exemplo, se o usuário fornecer o número 4, devem ser indicados os 4
+//primeiros números da sequência, que são 1, 1, 2 e 3.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int fib, temp, atual, i;
+
+
+    printf("Fibonacci de: ");
+    scanf("%d", &fib);
+
+    int fibonacci[fib];
+
+    temp = 0;
+    atual = 1;
+
+    for (i = 0; i < fib; i++){
+
+        fibonacci[i] = atual;
+
+        atual = atual + temp;
+
+        if (i == 0){
+            temp = atual;
+        } else {
+            temp = fibonacci[i - 1];
+        }
+
+    }
+
+    for (i = 0; i < fib; i++){
+        printf("%d", fibonacci[i]);
+        if (i == (fib - 1)){
+            printf("...\n");
+        } else {
+            printf(", ");
+        }
+    }
+
     return 0;
 }
